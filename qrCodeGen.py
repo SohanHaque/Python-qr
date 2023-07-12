70f1ca5b63f296e4b4bda3e7bac8cba456c74b9b
Md Sohanul Haque <111044439+SohanHaque@users.noreply.github.com>
2023-07-13 00:26:29 +0600

    Update qrCodeGen.py

--- qrCodeGen.py
+++ qrCodeGen.py
@@ -1,8 +1,21 @@
 import qrcode
+from tkinter import filedialog, Tk
 
-features = qrcode.QRCode(version=1, box_size=40, border=3)
-features.add_data('Sohan Loves Mimi')
-features.make(fit=True)
+data = input("Enter the data for the QR code: ")                                                        #User input
+box_size = int(input("Enter box size (default 40): ") or "40")
+border = int(input("Enter border size (default 3): ") or "3")
+fill_color = input("Enter fill color (default black): ") or "black"
+back_color = input("Enter background color (default white): ") or "white"
 
-generate_image = features.make_image(fill_color="black",back_color="white")
-generate_image.save('image001.png')
+image_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])  #Select path
+
+if not image_path:
+    print("No file path selected.")
+    exit()
+
+qr_code = qrcode.QRCode(version=1, box_size=box_size, border=border)                                    #Generate qrcode
+qr_code.add_data(data)
+qr_code.make(fit=True)
+
+qr_code.make_image(fill_color=fill_color, back_color=back_color).save(image_path)                       #Save image
+print("QR code image generated successfully.")
